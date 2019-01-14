89b33e6ec01ae1777e17f4e19d5e1144cb639f35
Galina Pitts <galina_pitts@icloud.com>
2019-01-14 00:41:18 -0800

    new3

--- config/connection.js
+++ config/connection.js
@@ -11,10 +11,10 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("error connecting!!!: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id !!!" + connection.threadId);
 });
 
 module.exports = connection;
