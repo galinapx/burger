60ed5b62e85bedec684dc5a87dae35b34b918861
Galina Pitts <galina_pitts@icloud.com>
2019-01-14 00:04:51 -0800

    12th

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT;
+//var PORT = process.env.PORT || 8000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
