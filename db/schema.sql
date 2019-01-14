806ce72f6444a13cd88b5a3d523c2bb723e4ab8c
Galina Pitts <galina_pitts@icloud.com>
2019-01-14 00:50:33 -0800

    new6

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
+CREATE DATABASE IF NOT EXISTS burger_db;
+USE burger_db;
 
 -- If the table already exists, remove it before trying to create the table again
 DROP TABLE IF EXISTS burgers;
